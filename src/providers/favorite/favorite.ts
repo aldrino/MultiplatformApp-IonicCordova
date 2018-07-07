8c4022249ac11e214e99e696218b584180d62ec6
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-07 22:38:42 +0530

    Assignment 3

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -15,20 +16,29 @@ export class FavoriteProvider {
 
   favorites: Array<any>;
 
-  constructor(public http: Http, private dishservice: DishProvider) {
+  constructor(public http: Http, private dishservice: DishProvider,
+    private storage: Storage) {
     console.log('Hello FavoriteProvider Provider');
-    this.favorites = [];
+    this.storage.get('favorites').then(favorites => {
+      if (favorites) {
+        this.favorites = favorites;
+      } else {
+        this.favorites = [];
+        this.storage.set('favorites', this.favorites);
+      }
+    })
   }
 
   addFavorite(id: number): boolean {
-    if (!this.isFavorite(id))
+    if (!this.isFavorite(id)) {
       this.favorites.push(id);
-    console.log('favorites', this.favorites);
+      this.storage.set('favorites', this.favorites);
+    }
     return true;
   }
 
   isFavorite(id: number): boolean {
-        return this.favorites.some(el => el === id);
+    return this.favorites.some(el => el === id);
   }
 
   getFavorites(): Observable<Dish[]> {
@@ -39,12 +49,13 @@ export class FavoriteProvider {
   deleteFavorite(id: number): Observable<Dish[]> {
     let index = this.favorites.indexOf(id);
     if (index >= 0) {
-      this.favorites.splice(index,1);
+      this.favorites.splice(index, 1);
+      this.storage.set('favorites', this.favorites);
       return this.getFavorites();
     }
     else {
       console.log('Deleting non-existant favorite', id);
       return Observable.throw('Deleting non-existant favorite' + id);
     }
-  }s
+  }
 }
