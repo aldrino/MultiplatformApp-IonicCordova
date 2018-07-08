0da278b306634f1338d0877ed8272beefb3df0ae
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-08 12:22:54 +0530

    Assignment 4

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -62,6 +62,25 @@ export class RegisterPage {
     });
   }
 
+  getFromLibrary() {
+    const options: CameraOptions = {
+      quality: 100,
+      correctOrientation: true,
+      allowEdit: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = imageData;
+      console.log(imageData);
+    }, (err) => {
+      console.log('Error obtaining picture')
+    });
+  }
+
   onSubmit() {
     console.log(this.registerForm.value);
     this.dismiss();
